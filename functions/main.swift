0754734fcc9dccd14944b41e6bd507cae339a966
athirapaul <athirapaul24@gmail.com>
2019-02-11 18:42:53 -0500

    Initial Commit

---
+++ functions/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  functions
+//
+//  Created by Athira Paul on 2019-02-11.
+//  Copyright © 2019 Athira Paul. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
